725a204e8a25c71c449e5770060d2e1265f7de80
yuzuki00 <144969224+yuzuki00@users.noreply.github.com>
2023-10-04 10:24:19 +0000

    [Finish] 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,14 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
@@ -10,4 +19,11 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+  
 end
